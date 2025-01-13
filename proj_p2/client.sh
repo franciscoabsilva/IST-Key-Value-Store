0e9a238610a44ce3349a185047d7f2b159b05f6e
Francisco Silva <francisco.brito.silva@tecnico.ulisboa.pt>
2025-01-13 21:04:03 +0000

    final project

--- proj_p2/client.sh
+++
@@ -1,5 +0,0 @@
-cd src
-cd client
-clear
-valgrind --leak-check=full  ./client "$1" "/home/franciscosilva/Area de Trabalho/SO/IST-Key-Value-Store/proj_p2/src/server/baba"
-
--- proj_p2/server.sh
+++
@@ -1,8 +0,0 @@
-# Run make
-make clean
-make
-
-cd src
-cd server
-clear
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --keep-debuginfo=yes ./kvs jobs 2 5 baba
